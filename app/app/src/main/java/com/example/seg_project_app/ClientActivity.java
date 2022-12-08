08cb6bb6c61f1fd4b417a7771c2d294a7d86cd13
Jadmgh <jmgha088@uottawa.ca>
2022-12-07 21:57:22 -0500

    Edits and designs

--- app/app/src/main/java/com/example/seg_project_app/ClientActivity.java
+++ app/app/src/main/java/com/example/seg_project_app/ClientActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -60,7 +59,7 @@ public class ClientActivity extends AppCompatActivity implements View.OnClickLis
                 startActivity(new Intent(ClientActivity.this, MainActivity.class));
                 return;
             case R.id.btnSearch:
-                Intent searchIntent = new Intent(ClientActivity.this, SearchView.class);
+                Intent searchIntent = new Intent(ClientActivity.this,SearchView.class);
                 searchIntent.putExtra("userInfo", clientInfo);
                 startActivity(searchIntent);
                 return;
